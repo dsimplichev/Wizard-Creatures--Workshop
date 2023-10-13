4e273a5bc8f25aec58ca7b450c67ab03ef73e292
dsimplichev <dsimplichev1948@gmail.com>
2023-10-13 12:55:52 +0100

    config router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 const { PORT } = require("./constants");
+const routes = require("./router");
+//Express config
+app.use(express.static(path.resolve(__dirname, "./public")))
+app.use(express.urlencoded({ extended: false}));
 
+//Routes
 app.get("/", (req, res) =>{
     res.send("Hello, Deni")
 });
+
+app.use(routes)
 
 app.listen(PORT, () => console.log(`Server is listening on port; ${PORT}`));
