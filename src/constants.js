fa025156eff03d52be3cc6ac35be2b2affb7dc65
dsimplichev <dsimplichev1948@gmail.com>
2023-10-14 15:07:03 +0100

    init mongoose and connect to DB

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-    PORT: 5002
+    PORT: 5002,
+    DB_URL: "mongodb://127.0.0.1:27017/wizard-creatures"
 }
 
  module.exports = CONFIG;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
 const handlebars = require("express-handlebars");
+const mongoose = require("mongoose");
 
-const { PORT } = require("./constants");
+const { PORT, DB_URL } = require("./constants");
 const routes = require("./router");
 
 const app = express();
@@ -15,7 +16,18 @@ app.engine('hbs', handlebars.engine({ extname: "hbs"}));
 app.set('view engine', "hbs");
 app.set('views', "src/views");
 
-//Routes
+async function dbConnect() {
+    await mongoose.connect(DB_URL);
+
+}
+
+dbConnect()
+.then(() => {
+    console.log("Successfully connected to the DB!");
+})
+.catch(err => 
+console.log(`Error while connecting to the DB. Error: ${err}`)
+);
 
 
 
