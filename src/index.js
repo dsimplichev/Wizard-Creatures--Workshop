837a8525b612761e1645535d6f23965d1a80e7da
dsimplichev <dsimplichev1948@gmail.com>
2023-10-14 13:52:42 +0100

    congigure view engine

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const app = express();
-const path = require("path")
+const path = require("path");
+const handlebars = require("express-handlebars");
+
 const { PORT } = require("./constants");
 const routes = require("./router");
+
+const app = express();
 //Express config
 app.use(express.static(path.resolve(__dirname, "./public")))
 app.use(express.urlencoded({ extended: false}));
+
+//Handlebars config
+app.engine('hbs', handlebars.engine({ extnam: "hbs"}));
+app.set('view engine', "hbs");
+app.set('views', "src/views");
 
 //Routes
 app.get("/", (req, res) =>{
