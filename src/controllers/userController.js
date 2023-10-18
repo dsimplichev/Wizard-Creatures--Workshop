fc32070d6a5600637f65a7287351870915b7cc8d
dsimplichev <dsimplichev1948@gmail.com>
2023-10-18 22:39:03 +0100

    validate password and login work

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -28,7 +28,12 @@ router.get('/login', (req, res) => {
     res.render("user/login")
 });
 
-router.post('/login', (req, res) =>{
+router.post('/login', async (req, res) =>{
+    console.log(req.body);
+    const {email,password} = req.body;
+
+    const user = await userService.login(email, password);
+    console.log({ user })
     res.redirect("/")
 });
 module.exports = router;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,17 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (email, password) => {
+const user = await User.findOne({ email });
+
+if(!user){
+    throw new Error("Invalid email or password!");
+}
+const isValid = await bcrypt.compare(password, user.password);
+if(!isValid) {
+
+ throw new Error("Invalid email or password!")
+}
+return user
+};
